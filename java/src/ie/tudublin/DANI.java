edfae6bdf41ef34e27fb7c1183c58d06cb3b51f2
Tom Keane <tomkeane282@gmail.com>
2023-04-26 18:16:03 +0100

    works for small file but not shakespeare

--- java/src/ie/tudublin/DANI.java
+++ java/src/ie/tudublin/DANI.java
@@ -23,66 +23,69 @@ public class DANI extends PApplet {
 	
 	public void setup() {
 		colorMode(HSB);
-		
+
 		//Load file
-		String[] lines = loadStrings("data/longertest.txt");
+		String[] lines = loadStrings("data/small.txt");
 		ArrayList<String> words = new ArrayList<String>();
 		
-
 		for (int i = 0; i < lines.length; i++) {
-            String[] tokens = split(lines[i], " ");
-            for (int j = 0; j < tokens.length; j++) {
-                if (j == tokens.length - 1) {
-                    //no word after
-                    words.add(tokens[j]);
-                } else {
-                    //Add word and next word
-                    words.add(tokens[j] + " " + tokens[j+1]);
-                }
-            }
-        }
+			String[] lineWords = split(lines[i], " ");
+			for (int j = 0; j < lineWords.length; j++) {
+				String w = lineWords[j].replaceAll("[^\\w\\s]",""); //punctuation
+				String lowerW = w.toLowerCase(); //lowercase
+				if (j == lineWords.length - 1) {
+					//no word after
+					words.add(lowerW);
+				} else {    //Add word
+					
+					String nextW = lineWords[j+1].replaceAll("[^\\w\\s]",""); //Remove punctuation
+					String lowerNextW = nextW.toLowerCase();   //lowercase
+					words.add(lowerW + " " + lowerNextW);
+				}
+			}
+		}
+		//array before removing copies
+		//println(words);
 
-		println(words);
+		/*
 
-		//COWABUNGA
+		Deleting duplicate words and appending the words that follow them
+
+		*/
+
 
 		ArrayList<String> result = new ArrayList<String>();
-        ArrayList<String> firstWords = new ArrayList<String>();
+		ArrayList<String> firstWords = new ArrayList<String>();
 
-        for (String element : words) {
-            String[] wordList = element.split(" ");
-            String firstWord = wordList[0];
+		for (String element : words) 
+		{
+			String[] wordList = element.split(" ");
+			String firstWord = wordList[0];
 
-            if (firstWords.contains(firstWord)) 
+			if (firstWords.contains(firstWord)) 
 			{
-                for (String existingElement : result) 
+				for (String existingElement : result) 
 				{
-                    if (existingElement.startsWith(firstWord)) 
+					if (existingElement.startsWith(firstWord))
 					{
-                        String newElement = existingElement;
-
-
-                        for (int i = 1; i < wordList.length; i++) 
+						String newElement = existingElement;
+						for (int i = 1; i < wordList.length; i++) 
 						{
-                            newElement += " " + wordList[i];
-                        }
-
-						
-                        result.set(result.indexOf(existingElement), newElement);
-                        break;
-                    }
-                }
-            } 
+							newElement += " " + wordList[i];
+						}
+						result.set(result.indexOf(existingElement), newElement);
+						break;
+					}
+				}
+			} 
 			else 
 			{
-                firstWords.add(firstWord);
-                result.add(element);
-            }
-        }
+				firstWords.add(firstWord);
+				result.add(element);
+			}
+		}
 
-        println(result);
-
-		
+		println(result);
 	}
 
 	public void keyPressed() {
